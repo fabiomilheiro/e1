987eb9946ca6757b284c6a3e6d6d0d51c4cd4869
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-08 10:37:43 +0100

    Created db context.

---
+++ E1.Web/E1.Web/DataAccess/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using E1.Web.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace E1.Web.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
