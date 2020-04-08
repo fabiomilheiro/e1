1d5bbb3f8d1fb2484af5b8d92a84d87330da0132
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-08 15:15:26 +0100

    Added market up for exact and partial name search. Added simple script to toggle between name search.

--- E1.Web/E1.Web/wwwroot/js/site.js
+++ E1.Web/E1.Web/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+(function () {
+    const exactSearch = $("#exact-search");
+    const partialSearch = $("#partial-search");
+
+    exactSearch.show();
+    partialSearch.hide();
+
+    $(".toggle-search").click(e => {
+        e.preventDefault();
+        if (exactSearch.is(":visible")) {
+            exactSearch.hide();
+            exactSearch.find("input").val("");
+            partialSearch.show();
+        } else {
+            exactSearch.show();
+            partialSearch.hide();
+            partialSearch.find("input").val("");
+        }
+    });
+})();
