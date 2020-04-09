c069d634f978fb70d9f6b41f785a2ba0f796a13c
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-04-09 14:39:37 +0100

    Added groups API endpoint.

---
+++ E1.Web/E1.Web/Areas/Api/Controllers/GroupsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using E1.Web.DataAccess;
+using E1.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E1.Web.Areas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IGroupRepository groupRepository;
+
+        public GroupsController(IGroupRepository groupRepository)
+        {
+            this.groupRepository = groupRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<GroupViewModel>> GetGroups()
+        {
+            var groups = this.groupRepository.GetGroups();
+
+            return Ok(groups.Select(g => new GroupViewModel
+            {
+                Id = g.Id,
+                Name = g.Name
+            }));
+        }
+    }
+}
---
+++ E1.Web/E1.Web/Models/GroupViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace E1.Web.Models
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
